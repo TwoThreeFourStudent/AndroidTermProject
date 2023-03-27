500dd41f8568afb953d33f02e420cde12c7224d1
Miguel Magpoc <mmagpoc@my.bcit.ca>
2023-03-27 14:24:05 -0700

    Maps added TODO map needs to zoom in to the current location

--- app/src/main/java/com/example/termproject/MainActivity.kt
+++ app/src/main/java/com/example/termproject/MainActivity.kt
@@ -7,6 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFrag()
 
     }
+
+    private fun addFrag(){
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerView_main, MapFragment())
+            .commit()
+    }
+
 }
--- app/src/main/java/com/example/termproject/MapFragment.kt
+++ app/src/main/java/com/example/termproject/MapFragment.kt
@@ -17,17 +17,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
